e9d5ae9c79275939cd2f29cef5372a5c19e68ba1
clairelyles <claire.lyles@gmail.com>
2014-12-05 11:42:58 -0800

    Changed public folder reference from p to P

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var Hashids = require("hashids");
 
 var app = express();
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/Public"));
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.get("/", function(req, res) {
